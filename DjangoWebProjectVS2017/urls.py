98e08572870e15f09374b28e2447b1de2d594546
AnderGomez14 <anderelmejor10@gmail.com>
2021-06-05 01:29:02 +0200

    Import todo

--- DjangoWebProjectVS2017/urls.py
+++ DjangoWebProjectVS2017/urls.py
@@ -13,6 +13,8 @@ import app.models
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
 from django.contrib import admin
+from django.conf.urls.static import static
+
 admin.autodiscover()
 
 urlpatterns = [# Examples:
